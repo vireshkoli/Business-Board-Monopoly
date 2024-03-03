6809775f78a0b7ff6490cbf93d2781fc7f344bed
Viresh <vireshkoli00@gmail.com>
2024-03-04 00:43:28 +0530

    Dashboard Member Section

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import "./Dashboard.css"
+import React from "react";
+import "./Dashboard.css";
+
+import memberimg from "../assets/Home Assets/profile.png"
+import detailsimg from "../assets/Dashboard Assets/detailsimg.png"
 
 function Dashboard() {
   return (
-    <div className='dashboardcontainer'>Hii I am Dashboard</div>
-  )
+    <div className="dashboardcontainer">
+      <div className="boardmembers">
+        <div className="members">
+          <h3>Person A</h3>
+          <img src={memberimg} alt="" />
+        </div>
+        <div className="members">
+          <h3>Person B</h3>
+          <img src={memberimg} alt="" />
+        </div>
+        <div className="members">
+          <h3>Person C</h3>
+          <img src={memberimg} alt="" />
+        </div>
+        <div className="members">
+          <h3>Person D</h3>
+          <img src={memberimg} alt="" />
+        </div>
+        <div className="members">
+          <h3>Person E</h3>
+          <img src={memberimg} alt="" />
+        </div>
+        <div className="viewdetails">
+          <h3>VIEW DETAILS</h3>
+          <img src={detailsimg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
